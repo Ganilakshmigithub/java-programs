2ad4797c6b2f822b203d570dcfa3c81f0ecba7fe
Bheri Gani Lakshmi <112390375+Ganilakshmigithub@users.noreply.github.com>
2024-11-18 14:16:14 +0530

    Delete crud directory

--- crud/src/main/java/Spring/boot/crud/repos/PhoneNumberRepo.java
+++
@@ -1,8 +0,0 @@
-package Spring.boot.crud.repos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Spring.boot.crud.entities.*;
-
-public interface PhoneNumberRepo  extends JpaRepository<PhoneNumber,Long>{
-    
-}
--- crud/src/main/java/Spring/boot/crud/security/JWTConstant.java
+++
@@ -1,8 +0,0 @@
-package Spring.boot.crud.security;
-
-public class JWTConstant {
-
-    public static String HEADER = "Authorization";
-    public static String SECRET_KEY = "jbavlbadlvnogdauysnafuksbjshitsdas";
-
-}
